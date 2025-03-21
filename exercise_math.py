bf430d8941883af1e95e1f8db9084b8630bc333c
justinamoran <jmoran1@mail.austral.edu.ar>
2025-03-21 12:27:45 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,17 @@
 def math():
     a = 57
     b = 7
+    z = a+b
+    y = a-b
+    x = a*b
+    w = a//b
+    v = a%b
+    u = a/b
+    t = (a+b)/2
+    print(z)
+    print(y)
+    print(x) 
+    print(t)  
+    print(w)
+    print(v)
+    print(u)
